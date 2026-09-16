5d39a7132742dc607ac7d567e3f7eadfcf355f2a
darainfo <varsqlinfo@gmail.com>
2026-09-16 09:52:24 +0900

    field 마지막 디자인 틀어진거 수정.

--- vite.common.js
+++ vite.common.js
@@ -1,6 +1,6 @@
-import path from 'path';
-import banner from 'vite-plugin-banner';
-import packageJson from './package.json';
+import path from "path";
+import banner from "vite-plugin-banner";
+import packageJson from "./package.json";
 
 const topBanner = `/*!
 * ${packageJson.name} v${packageJson.version}
@@ -8,27 +8,27 @@ const topBanner = `/*!
 * Licensed ${packageJson.license}
 */`;
 
-const moduleName = 'daracl.form';
+const moduleName = "daracl.form";
 
 export const MODULE_NAME = moduleName;
 
 /** 공통 asset 설정 */
 export const createAssetFileNames = (isProd) => {
   return (assetInfo) => {
-    if (assetInfo.name.endsWith('.css')) {
+    if (assetInfo.name.endsWith(".css")) {
       return isProd ? `${MODULE_NAME}.min.[ext]` : `${MODULE_NAME}.[ext]`;
     }
-    return 'assets/[name].[ext]';
+    return "assets/[name].[ext]";
   };
 };
 
 export const commonConfig = {
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@t': path.resolve(__dirname, 'src/types'),
+      "@": path.resolve(__dirname, "src"),
+      "@t": path.resolve(__dirname, "src/types"),
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 
   plugins: [banner(topBanner)],
@@ -37,11 +37,11 @@ export const commonConfig = {
     APP_VERSION: JSON.stringify(packageJson.version),
   },
   server: {
-    host: '0.0.0.0',
-    port: 4178,
+    host: "0.0.0.0",
+    port: 5501,
     open: "/uitest/index.html",
     watch: {
-      ignored: ['!**/src/**'],
+      ignored: ["!**/src/**"],
     },
   },
 };
